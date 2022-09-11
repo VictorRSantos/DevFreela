e10f659fca81c0f25a100d7ecc7a841927eafe50
Victor R dos Santos <vrsisa@hotmail.com>
2022-09-10 22:38:21 -0300

    testando branch

--- DevFreela/DevFreela.API/Controllers/ProjetsController.cs
+++ DevFreela/DevFreela.API/Controllers/ProjetsController.cs
@@ -1,20 +1,59 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Infrastructure;
-using Microsoft.AspNetCore.Mvc.Routing;
+﻿using DevFreela.API.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DevFreela.API.Controllers
 {
     [Route("api/projects")]
     public class ProjetsController : ControllerBase
     {
-
-        
-
+        // api/projects?query=net core
         [HttpGet]
-        public IActionResult Get()
+        public IActionResult Get(string query)
         {
             return Ok();
         }
+
+        // api/projects/3
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok();
+        }
+
+        
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateProjectModel createProject)
+        {
+            if (createProject.Title.Length > 50)
+                return BadRequest();
+
+            return CreatedAtAction(nameof(GetById), new {id = createProject.Id }, createProject);
+        }
+
+        // api/projects/2
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] UpdateProjectModel updateProject)
+        {
+            if (updateProject.Description.Length > 200)
+            {
+                return BadRequest();
+            }
+
+            return NoContent();
+
+        }
+
+        // api/projects/3 DELETE
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+
+            return NoContent();
+        }
+
+
+
+
 
 
 
