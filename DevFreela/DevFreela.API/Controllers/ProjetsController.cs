0a770061e62a7bde77c4d508a048043f1dd0838a
Victor R dos Santos <vrsisa@hotmail.com>
2022-09-10 21:51:27 -0300

    first commit

---
+++ DevFreela/DevFreela.API/Controllers/ProjetsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Routing;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/projects")]
+    public class ProjetsController : ControllerBase
+    {
+
+        
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+
+
+    }
+}
