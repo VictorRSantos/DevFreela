0b9bd9022b4344cd6aa4940d482b4c4066e807ec
Victor Rodrigues <vrsisa@hotmail.com>
2022-10-29 16:49:41 -0300

    Foi criado classe Login Command

--- DevFreela/DevFreela.Application/Commands/LoginUser/LoginUserCommandHanlder.cs
+++ DevFreela/DevFreela.Application/Commands/LoginUser/LoginUserCommandHanlder.cs
@@ -1,9 +1,8 @@
 ﻿using DevFreela.Application.ViewModels;
+using DevFreela.Core.Repositories;
+using DevFreela.Core.Services;
 using MediatR;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -11,11 +10,34 @@ namespace DevFreela.Application.Commands.LoginUser
 {
     public class LoginUserCommandHanlder : IRequestHandler<LoginUserCommand, LoginUserViewModel>
     {
-
-
-        public Task<LoginUserViewModel> Handle(LoginUserCommand request, CancellationToken cancellationToken)
+        private readonly IAuthService _authService;
+        private readonly IUserRepository _userRepository;
+        public LoginUserCommandHanlder(IAuthService authService, IUserRepository userRepository)
         {
-            throw new NotImplementedException();
+            _authService = authService;
+            _userRepository = userRepository;
+        }
+
+        public async Task<LoginUserViewModel> Handle(LoginUserCommand request, CancellationToken cancellationToken)
+        {
+            // Utilizar o mesmo algoritmo para criar o hash dess senha
+            var passwrodHash = _authService.ComputeSha256Hash(request.Password);
+
+            // Buscar no  meu banco de dados um User que tenha meu e-mail e minha senha em formatado hash
+            var user = await _userRepository.GetUserByEmailAndPasswordAsync(request.Email, passwrodHash);
+
+            // Se não existir, erro no login
+            if (user == null)
+            {
+                return null;
+            }
+
+            // Se existir, gera o token usuando os dados do usuário
+            var token = _authService.GenerateJwtToken(user.Email, user.Role);
+
+            var loginViewViewModel = new LoginUserViewModel(user.Email,token);
+
+            return loginViewViewModel;
         }
     }
 }
